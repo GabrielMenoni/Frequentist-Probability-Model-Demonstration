b0918a75c03e091bd760a70ef92f499af22fa4cf
Gabriel Fernandes Menoni <contato.gmenoni@gmail.com>
2023-12-05 09:34:48 -0300

    Added Comments

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
+# Imports
 import matplotlib.pyplot as plt
 import random as rd
 
+# Chart configuration
 plt.xlabel("Qtd de jogadas / Throws")
 plt.ylabel("Porcentagem / Percentage")
 plt.title("""Probabilidade de ser "Cara" / Probability of being "Heads" """)
@@ -9,12 +11,14 @@ plt.axhline(y = 50, color = 'black', linestyle = '-')
 
 plot = 0
 
+# Main loop (Create 5 different plots)
 while plot < 5:
     listx = []
     listy = []
     qtd = 0
     porcent = 0
     
+    # Loop to simulate 5000 throws
     for i in range (1, 5001):
         if rd.choice([0, 1]) == 0:
             qtd += 1
@@ -22,8 +26,9 @@ while plot < 5:
         listx.append(i)
         listy.append(porcent)
         
-        
+    # Add to plot and reset values
     plt.plot(listx, listy)    
     plot += 1
 
+# Show Final plot
 plt.show()
